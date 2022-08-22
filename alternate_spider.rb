872466e5c2b95886f41e3fe1376f61169f784bb6
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2022-08-22 18:07:04 +0200

    Oops

--- alternate_spider.rb
+++ alternate_spider.rb
@@ -61,5 +61,6 @@ alternate.crawl(query: 'RASPBERRY PI', type: 1703)
 if !alternate.available?
   message = 'New Pi’s available!'
   key = ARGV[0]
+  puts key.chars.first(3)
   Notifier.new(message: message, title: 'Alternate', key: key).send
 end
